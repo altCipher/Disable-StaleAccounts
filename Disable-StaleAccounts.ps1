ddc3a5a03b535bd2d7381348152c2911725a5bed
Tony Cavella <acavella@gmail.com>
2019-12-29 07:45:00 -0500

    Correct inline help format

--- Disable-StaleAccounts.ps1
+++ Disable-StaleAccounts.ps1
@@ -1,12 +1,12 @@
 <#
-	.SYNOPSIS
-		 Disables stale Active Directory accounts.
-	.DESCRIPTION
-         Disables Active Directory accounts that have been inactive for 
-         a specified time period.  A custom AD parameter is set (DateDisabled).
-         Accounts that remain inactive will be deleted automatically 
-         after a specified time period.  A comma seperated value (CSV) 
-         output is generated to record accounts disabled and deleted.
+    .SYNOPSIS
+        Disables stale Active Directory accounts.
+    .DESCRIPTION
+        Disables Active Directory accounts that have been inactive for 
+        a specified time period.  A custom AD parameter is set (DateDisabled).
+        Accounts that remain inactive will be deleted automatically 
+        after a specified time period.  A comma seperated value (CSV) 
+        output is generated to record accounts disabled and deleted.
 	.PARAMETER Path
         Specify the path of text (.txt) file contianing list of OUs to 
         monitor.
